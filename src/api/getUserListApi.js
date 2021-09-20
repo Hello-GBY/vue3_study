8aa0afad74b250aa3cdf724374d5b31c752b3f03
gby <nameGBY@163.com>
2021-09-20 14:33:28 +0800

    用户请求

---
+++ src/api/getUserListApi.js
@@ -0,0 +1,33 @@
+// import axios from 'axios';
+
+const modelData = [{
+  name: 'gby',
+  value: '1',
+  id: '1'
+},{
+  name: 'qwe',
+  value: '1',
+  id: '2'
+},{
+  name: 'asd',
+  value: '1',
+  id: '3'
+},{
+  name: 'zxc',
+  value: '1',
+  id: '4'
+},{
+  name: 'rty',
+  value: '1',
+  id: '5'
+},{
+  name: 'ghj',
+  value: '1',
+  id: '6'
+}]
+
+export function getUserListApi(){
+  console.log('请求数据===');
+  // 模拟数据
+  return modelData
+}
---
+++ src/views/myTest.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <input v-model="queryName"/>
+    <div v-for="user in userList" :key="user.id">{{user.name}}</div>
+  </div>
+</template>
+
+<script>
+/**
+ * 一个模拟用户仓库查询
+ */
+// 组合api 练习
+import {onMounted, ref, watch} from 'vue'
+import {getUserListApi} from '@/api/getUserListApi'
+export default {
+  setup(props){
+    // user列表
+    const userList = ref([]);
+    const queryName = ref('');
+
+    // 请求数据
+    const getUserList = () => {
+      // userList.value = await getuserListApi(props.user)
+      userList.value = getUserListApi(props.user)
+    }
+
+    // 请求数据处理
+    const  filterUserList= () => {
+      console.log('进');
+      userList.value = userList.value.filter(res => res.name.indexOf(queryName.value) > -1)
+    }
+
+    // 初始化的时候请求数据
+    onMounted(getUserList);
+
+    // 用户名改变时触发
+    const queryNameChange = function () {
+      let userArr = getUserList();
+    }
+
+    watch(queryName, filterUserList)
+
+    // 返回实例
+    return {
+      userList: userList,
+      queryName
+    }
+
+    // 计算属性
+
+  }
+}
+</script>
+
+<style>
+
+</style>
