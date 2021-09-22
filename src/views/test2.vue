13c155f7ed763a5992125cef293febe21fb3dba8
gby <1565297930@qq.com>
2021-09-22 18:35:06 +0800

    fix

--- src/views/test2.vue
+++ src/views/test2.vue
@@ -1,5 +1,15 @@
 <template>
   <h2> reative 的使用</h2>
+  <div>
+      {{user.name}}
+      <div>
+        {{user.property.stuta}}
+      </div>
+      <div>
+        {{user.property.learning}}
+      </div>
+  </div>
+  <button @click="updateUser">快去玩</button>
 </template>
 <script lang="ts">
 import { defineComponent, reactive } from "vue";
@@ -9,9 +19,20 @@ export default defineComponent({
     setup(){
         // reactive 把复杂数据变成响应式数据
         const user = reactive({
-            name: '小明'
+            name: '小郭',
+            property: {
+                stuta: 'codeing',
+                learning: 'vue3'
+            }
         });
-        return {};
+        // 方法
+        const updateUser = () => {
+            user.property.stuta = 'playing'
+        }
+        return {
+            user,
+            updateUser
+        };
     }
 })
 </script>
